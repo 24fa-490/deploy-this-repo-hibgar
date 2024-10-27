e7969063dcd62d945bcac8e729371491850e7b02
Hiba Mughal <hibgar@gmail.com>
2024-10-26 23:21:25 -0700

    Revert "changed import"
    This reverts commit 2748efe0dc5895850ca1f84a9bc5c7a7f801c106.

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,10 +1,8 @@
 
 import postgres from 'postgres'
 
-import meta.env
-
 // see env variables in .env
-//import {PGCONNECT} from '$env/static/private';
+import {PGCONNECT} from '$env/static/private';
 
 const sql = postgres(PGCONNECT, {} )
 
