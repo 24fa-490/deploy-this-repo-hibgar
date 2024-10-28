95a905df50951fd3ad303abf3f50507cdd87d0c8
Hiba Mughal <hibgar@gmail.com>
2024-10-27 18:30:11 -0700

    import SQL from vercel

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,13 +1,17 @@
 
-import postgres from 'postgres'
+//import postgres from 'postgres'
 
 // see env variables in .env
 //import {PGCONNECT} from '$env/static/private';
 
-import * as env from '$env/dynamic/private';
+//import * as env from '$env/dynamic/private';
 
-const PGCONNECT = env.DATABASE_URL; // My variable name here was DATABASE_URL not PGCONNECT
+//const PGCONNECT = env.DATABASE_URL; // My variable name here was DATABASE_URL not PGCONNECT
 
-const sql = postgres(PGCONNECT)
+//const sql = postgres(PGCONNECT)
 
-export default sql;
+//export default sql;
+
+
+import { sql } from "@vercel/postgres";
+
