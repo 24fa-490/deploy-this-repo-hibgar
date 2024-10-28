40a3caab70a55572dd307b8aecbce079933e3f6f
Hiba Mughal <hibgar@gmail.com>
2024-10-27 19:53:04 -0700

    import PGCONNECT using process.env

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -4,9 +4,9 @@ import postgres from 'postgres'
 // see env variables in .env
 //import {PGCONNECT} from '$env/static/private';
 
-import * as env from '$env/dynamic/private';
+//import * as env from '$env/dynamic/private';
 
-//const PGCONNECT = env.DATABASE_URL; // My variable name here was DATABASE_URL not PGCONNECT
+const PGCONNECT = process.env.PGCONNECT; // My variable name here was DATABASE_URL not PGCONNECT
 
 const sql = postgres(PGCONNECT)
 
